bd0bf97c5b95f2dbf764396aba0d48c930d5d414
CollinClifford <collinclifford@ymail.com>
2022-03-24 09:21:12 -0700

    fixed wordScrambler

--- src/App.js
+++ src/App.js
@@ -5,62 +5,56 @@ import "./App.css";
 function App() {
   const [sentenceId, setSentenceId] = useState(1);
   const [sentence, setSentence] = useState("");
+  const [error, setError] = useState();
 
   let shuffled = [];
   let tempArray = [];
   let array2 = [];
-  useEffect(() => {
-    async function loadProfiles(url) {
-      const response = await fetch(url);
-      const json = await response.json();
-      setSentence(Object.values(json.data).toString());
-    }
-    loadProfiles(`https://api.hatchways.io/assessment/sentences/${sentenceId}`);
-  }, [sentenceId]);
 
+  // async function loadProfiles(url) {
+  //   const response = await fetch(url);
+  //   const json = await response.json();
+  //   console.log(json);
+  //   setSentence(Object.values(json.data).toString());
+  // }
+  async function loadSentence(url) {
+    const abortController = new AbortController();
+    setError(null);
+    const response = await fetch(url);
+    const json = await response.json();
+    // console.log(json);
+    setSentence(Object.values(json.data).toString());
+    return () => abortController.abort();
+  }
+  useEffect(() => {
+    loadSentence(`https://api.hatchways.io/assessment/sentences/${sentenceId}`);
+  }, []);
+
+  // useEffect(() => {
+  //   loadProfiles(`https://api.hatchways.io/assessment/sentences/${sentenceId}`);
+  // }, []);
+  let mutatedWord = "";
   function wordScrambler(sentence) {
-    console.log(sentence);
+    // let mutatedWord = "";
+    let newSentence = "";
     let array = sentence.split(" ");
     for (let i = 0; i < array.length; i++) {
-      if (array[i].length > 2) {
-        array2 = array[i].split("");
-        // console.log(array2);
-        for (let j = 1; j < array2.length - 1; j++) {
-          tempArray.push(array2[j]);
-          // tempArray = tempArray.sort(() => Math.random() - 0.5);
-          // console.log(tempArray);
-        }
-        shuffled.push(array2[0], ...tempArray, array2[array2.length - 1]);
-      } else {
-        shuffled.push(array[i]);
+      array2 = array[i].split("");
+      if (array2.length <= 2) {
+        mutatedWord = array2.join("");
+      } else if (array2.length > 2) {
+        let firstLetter = array2[0];
+        let lastLetter = array2[array2.length - 1];
+        let mutableArray = array2;
+        mutableArray.pop();
+        mutableArray.shift();
+        mutableArray.sort(() => Math.random() - 0.5);
+        let middle = mutableArray.join("");
+        mutatedWord = firstLetter + middle + lastLetter;
       }
+      newSentence += mutatedWord + " ";
     }
-    console.log(shuffled.join(""));
-    // return sentence.split(" ").forEach((word) => {
-    // if (word.length > 2) {
-    // word = word
-    //   .split("")
-    //   .sort(() => Math.random() - 0.5)
-    //   .join("");
-    // return word;
-    // for (let i = 1; i < word.length - 1; i++) {
-    //   shuffled.push(word[i]);
-    //   shuffled = shuffled.sort(() => Math.random() - 0.5);
-    // }
-    // shuffled.unshift(word[0]);
-    // shuffled.push(word[word.length - 1]);
-    // return word;
-    // console.log(word);
-    //   return word;
-    // } else {
-    // return word;
-    // }
-    // });
-    // return sentence;
-  }
-
-  if (sentence) {
-    wordScrambler(sentence);
+    return newSentence;
   }
 
   return (
