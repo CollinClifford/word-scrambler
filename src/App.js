8ce0754052051467c7d6e63bd543c48c86b43794
CollinClifford <collinclifford@ymail.com>
2022-03-25 09:51:06 -0700

    still very much broken but closer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [sentenceId, setSentenceId] = useState(1);
-  const [sentence, setSentence] = useState("");
-  const [answer, setAnswer] = useState("");
-  const [error, setError] = useState();
-  const [score, setScore] = useState(0);
+  let [sentenceId, setSentenceId] = useState(1);
+  let [sentence, setSentence] = useState("");
+  const [answer, setAnswer] = useState([]);
+  let [score, setScore] = useState(0);
 
   let array2 = [];
 
   async function loadSentence(url) {
     const abortController = new AbortController();
-    setError(null);
     const response = await fetch(url);
     const json = await response.json();
     setSentence(Object.values(json.data).toString());
@@ -21,10 +18,10 @@ function App() {
   }
   useEffect(() => {
     loadSentence(`https://api.hatchways.io/assessment/sentences/${sentenceId}`);
-  }, []);
+  });
 
-  let mutatedWord = "";
   function wordScrambler(sentence) {
+    let mutatedWord = "";
     let newSentence = "";
     let array = sentence.split(" ");
     for (let i = 0; i < array.length; i++) {
@@ -45,46 +42,70 @@ function App() {
     }
     return newSentence.trim();
   }
-  function changeHandler({ target }) {
-    setAnswer(target.value);
+
+  function changeHandler(value, index) {
+    if (answer.length >= 1) {
+      setAnswer((previousAnswer) => [...previousAnswer, value]);
+    } else {
+      setAnswer(value);
+    }
   }
-  console.log(answer)
+
+  const scrambledSentence = wordScrambler(sentence);
+
+  function submitHandler(event) {
+    event.preventDefault();
+    if (
+      answer.join("").toLowerCase() ===
+      sentence.split(" ").join("").toLowerCase()
+    ) {
+      setSentenceId(sentenceId++);
+      setScore(score++);
+    } else {
+      alert("Try again");
+    }
+  }
 
   return (
     <div className="container">
-      <h1>{sentence && wordScrambler(sentence)}</h1>
+      <h1>{scrambledSentence}</h1>
       <p>Guess the sentence! Start typing.</p>
       <p>The yellow blocks are meant for spaces</p>
       <h2>Score: {score}</h2>
-      <form>
+      <form onSubmit={submitHandler}>
         {sentence.split("").map((letter, index) => {
-
-          // fix this!!!!
-          return (
-            <input
-              type="text"
-              id="index"
-              name="letter"
-              value={answer}
-              onChange={changeHandler}
-              max="1"
-            ></input>
-          );
+          if (letter === " ") {
+            return (
+              <>
+                <input
+                  type="text"
+                  id={index}
+                  name="letter"
+                  className="space"
+                  disabled
+                  maxLength="1"
+                ></input>
+                <br />
+              </>
+            );
+          } else {
+            return (
+              <input
+                type="text"
+                id={index}
+                name="letter"
+                value={letter[index]}
+                onChange={(event) => changeHandler(event.target.value, index)}
+                maxLength="1"
+              ></input>
+            );
+          }
         })}
+        <br />
         <input type="submit" value="Submit" />
       </form>
     </div>
   );
 }
-
-/*
-<form action="/action_page.php">
-  <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname"><br><br>
-  <label for="lname">Last name:</label>
-  <input type="text" id="lname" name="lname"><br><br>
-  <input type="submit" value="Submit">
-</form>
-*/
 
 export default App;
